251b17274434efd8f4387f4fa305317c44dd8b34
Eko Saputro <eko.saputro@hypefast.id>
2022-12-28 17:27:58 +0700

    add command to search game

---
+++ helpers.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+import os
+import gspread
+import traceback
+import pandas as pd
+from dotenv import load_dotenv
+from gspread_dataframe import get_as_dataframe
+load_dotenv()
+
+
+gc = gspread.service_account(filename=os.getenv("CREDENTIALS_FILE"))
+sh = gc.open_by_key(os.getenv("SPREADSHEET_KEY"))
+gamepass_sheet = sh.worksheet(os.getenv("GAMEPASS_SHEET"))
+
+
+def search_game_all(game):
+
+    # base text
+    text = f"Game : {game}\n"
+
+    # get data from google sheet
+    gamepass = get_as_dataframe(gamepass_sheet)
+    gamepass = gamepass.astype(str).apply(lambda x: x.str.lower())
+
+    try:
+        # check if game is available on Game Pass Console
+        console = [i for i in gamepass['Console Games'].tolist() if game in i]
+        # check if game will available on Game Pass
+        soon = [i for i in gamepass['Coming Soon'].tolist() if game in i]
+
+        if len(console) > 0 and len(soon) > 0:
+            text += "\n* Game Pass: \n - " + '\n - '.join(console)
+            text += "\n\n* Coming on Game Pass: \n - " + '\n - '.join(soon)
+        elif len(console) > 0 and len(soon) == 0:
+            text += "\n* Game Pass: \n - " + '\n - '.join(console)
+        elif len(console) == 0 and len(soon) > 0:
+            text += "\n* Game Pass: Not available\n"
+            text += "\n* Coming on Game Pass: \n - " + '\n - '.join(soon)
+        else:
+            text += "\n* Game Pass : Not available\n"
+            text += "\n* Coming on Game Pass : Not soon\n"
+
+    except Exception as e:
+        print(traceback.format_exc())
+        return "Oops, there is an error: " + str(e)
+
+    return text
--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from telegram import *
 from telegram.ext import *
 from chatgpt3 import ChatGPT3
 from dotenv import load_dotenv
+import helpers
 load_dotenv()
 
 
@@ -15,6 +16,13 @@ def handle_message(update, context):
     text = str(update.message.text).lower()
     response = gpt3.talk(text)
     update.message.reply_text(response)
+
+
+def search_game_all(update, context):
+    game = (' ').join(i for i in context.args[:])
+    update.message.reply_text(f"Searching for {game} ...")
+    summary = helpers.search_game_all(game)
+    update.message.reply_text(summary)
 
 
 def error_message(update, context):
@@ -26,6 +34,8 @@ def main():
 
     updater = Updater(os.getenv("BOT_API_KEY"), use_context=True)
     dp = updater.dispatcher
+
+    dp.add_handler(CommandHandler("search_game_all", search_game_all))
 
     dp.add_handler(MessageHandler(Filters.text, handle_message))
     dp.add_error_handler(error_message)
